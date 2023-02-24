0440f3191f7de4677ce836fce02c1096277802fe
Thura-htet <lxgsts@gmail.com>
2023-02-24 04:27:46 +0100

    initial commit
    Initial commit to publish portfolio (and test the email client)

---
+++ script/script.js
@@ -0,0 +1,26 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// gsap.utils.toArray(".test").forEach((test, i) => {
+//   ScrollTrigger.create({
+//     trigger: test,
+//     start: "top top", 
+//     pin: true, 
+//     pinSpacing: false 
+//   });
+// });
+
+// gsap.to("#fin", 
+//     {
+//         scrollTrigger: {
+//             trigger: "#fin",
+//             start: "center top",
+//             pin: true,
+//             pinSpacing: false,
+//             marker: true
+//         }
+//     })
+
+
+// ScrollTrigger.create({
+//   snap: 1 / 4 // snap whole page to the closest section!
+// });
