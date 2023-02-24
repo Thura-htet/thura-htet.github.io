5829ec94dad66d700e7544e1ebd3d526a0c637e4
Thura-htet <lxgsts@gmail.com>
2023-02-24 07:37:26 +0100

    turned the elements into projector style containers. the email client end point works now.

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
-gsap.registerPlugin(ScrollTrigger);
+AOS.init();
+// gsap.registerPlugin(ScrollTrigger);
 
 // gsap.utils.toArray(".test").forEach((test, i) => {
 //   ScrollTrigger.create({
@@ -9,11 +10,11 @@ gsap.registerPlugin(ScrollTrigger);
 //   });
 // });
 
-// gsap.to("#fin", 
+// gsap.to("#contact", 
 //     {
 //         scrollTrigger: {
 //             trigger: "#fin",
-//             start: "center top",
+//             start: "top top",
 //             pin: true,
 //             pinSpacing: false,
 //             marker: true
@@ -21,6 +22,85 @@ gsap.registerPlugin(ScrollTrigger);
 //     })
 
 
-// ScrollTrigger.create({
-//   snap: 1 / 4 // snap whole page to the closest section!
-// });
+// gsap.to("#welcome",
+//     {
+//         scrollTrigger: {
+//             trigger: "#welcome",
+//             start: "bottom bottom",
+//             pin: true,
+//             pinSpacing: false,
+//         }
+//     })
+
+// gsap.to("#upper-cover",
+//     {
+//         scrollTrigger: {
+//             trigger: "#welcome",
+//             start: "bottom bottom",
+//             pin: true,
+//             pinSpacing: false,
+//         },
+//         y: 20
+//     });
+
+// gsap.to(".info-projector",
+//     {
+//         opacity: 1,
+//         ease: 'none',
+//         scrollTrigger: {
+//             trigger: "#personal-info",
+//             start: "top top",
+//             end: "bottom top",
+//             scrub: true
+//         }
+//     });
+
+// gsap.to(".info-projector",
+//     {
+//         opacity: 0,
+//         ease: 'none',
+//         scrollTrigger: {
+//             trigger: ".personalx",
+//             start: "top top",
+//             end: "bottom top",
+//             pin: true,
+//             pinSpacing: false,
+//             scrub: true
+//         }
+//     });
+
+// gsap.to(".parcours-projector",
+//     {
+//         opacity: 1,
+//         ease: 'none',
+//         scrollTrigger: {
+//             trigger: "#personal-info",
+//             start: "top top",
+//             end: "bottom top",
+//             scrub: true
+//         }
+//     });
+
+// gsap.to(".projects-projector",
+//     {
+//         opacity: 1,
+//         ease: 'none',
+//         scrollTrigger: {
+//             trigger: "#personal-info",
+//             start: "top top",
+//             end: "bottom top",
+//             scrub: true
+//         }
+//     });
+
+// gsap.to(".competences-projector",
+//     {
+//         opacity: 1,
+//         ease: 'none',
+//         scrollTrigger: {
+//             trigger: "#personal-info",
+//             start: "top top",
+//             end: "bottom top",
+//             scrub: true
+//         }
+//     });
